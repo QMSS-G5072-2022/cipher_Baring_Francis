866c1f0a6002adef12865af6f469706f5c1f818d
Frank Baring <frankbaring@gmail.com>
2022-11-02 01:37:08 -0400

    Commit after running successful pytest

--- cipher_fb2493/tests/test_cipher_fb2493.py
+++ cipher_fb2493/tests/test_cipher_fb2493.py
@@ -1,1 +1,8 @@
 from cipher_fb2493 import cipher_fb2493
+
+def test_cipher_with_word():
+    assert cipher_fb2493.cipher("Hello",3,True) == 'Khoor', "Should be 'Khoor'"
+
+def test_cipher_with_negative_shift():
+    assert cipher_fb2493.cipher("Hello",-3,True) == 'Ebiil', "Should be 'Ebiil'"
+   
